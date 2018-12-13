5dbb193cb17222303015c707536a20f26bf5fe29
iman4000 <imanilbag2@gmail.com>
2018-12-13 10:55:45 -0800

    create obj_box

--- app.py
+++ app.py
@@ -4,6 +4,30 @@ from tkinter import scrolledtext
 from phpserialize import *
 
 
+#Test class for make obj_box
+class graphic_obj_basic:
+        def __init__(self, master):
+        '''
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+        self.button1.pack()
+        self.frame.pack()
+        '''    
+        #second way
+
+        w = Label(root, text="red", bg="red", fg="white")
+        w.pack(padx=5, pady=10, side=LEFT)
+        w = Label(root, text="green", bg="green", fg="black")
+        w.pack(padx=5, pady=20, side=LEFT)
+        w = Label(root, text="blue", bg="blue", fg="white")
+        w.pack(padx=5, pady=20, side=LEFT)
+
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Demo2(self.newWindow)
+        
+
 window = Tk()
  
 window.title("Welcome to Iman app")
